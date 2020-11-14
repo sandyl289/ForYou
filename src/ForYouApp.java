308038562a91d4fa9f712bd73e14564579063a46
Sandy L <70775435+sandyl289@users.noreply.github.com>
2020-11-14 00:13:21 -0500

    Add Hello label

--- src/ForYouApp.java
+++ src/ForYouApp.java
@@ -11,13 +11,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
-import java.awt.Font;
-import java.awt.Color;
 import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 public class ForYouApp extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtEnterYourName;
 
 	/**
 	 * Launch the application.
@@ -46,12 +47,6 @@ public class ForYouApp extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblWelcome = new JLabel("WELCOME TO OUR PROJECT");
-		lblWelcome.setForeground(new Color(0, 206, 209));
-		lblWelcome.setFont(new Font("Century Gothic", Font.PLAIN, 54));
-		lblWelcome.setBounds(298, 43, 769, 101);
-		contentPane.add(lblWelcome);
-		
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1370, 750);
@@ -61,5 +56,32 @@ public class ForYouApp extends JFrame {
 		setLocationRelativeTo(null);
 		
 		setTitle("For You ~~");
+		
+		JButton enterButton = new JButton("Enter");
+		enterButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("Enter button pressed");
+			}
+		});
+		enterButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		enterButton.setBounds(174, 600, 97, 25);
+		contentPane.add(enterButton);
+		
+		txtEnterYourName = new JTextField();
+		txtEnterYourName.setForeground(new Color(0, 206, 209));
+		txtEnterYourName.setText("Enter your name");
+		txtEnterYourName.setBounds(351, 263, 150, 25);
+		contentPane.add(txtEnterYourName);
+		txtEnterYourName.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Hello! :)  What is your name?");
+		lblNewLabel.setFont(new Font("Century Gothic", Font.PLAIN, 20));
+		lblNewLabel.setForeground(new Color(0, 206, 209));
+		lblNewLabel.setBounds(54, 235, 416, 75);
+		contentPane.add(lblNewLabel);
 	}
 }
