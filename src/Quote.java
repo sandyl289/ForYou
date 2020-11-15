cec2f9afc8bbe775861d78ba76b735ade8879294
yumicha1 <36577595+yumicha1@users.noreply.github.com>
2020-11-14 19:59:16 -0500

    change the size of the window quote

--- src/Quote.java
+++ src/Quote.java
@@ -30,7 +30,7 @@ public class Quote extends JFrame {
 	 */
 	public Quote() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 751, 501);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
