8c21472e0ad5889170ad2a08f5b2c6f57fe1af99
yumicha1 <36577595+yumicha1@users.noreply.github.com>
2020-11-14 19:55:56 -0500

    add a window quote as a class

--- src/ForYouApp.java
+++ src/ForYouApp.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,17 +10,19 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
-import java.awt.Color;
-import java.awt.Font;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class ForYouApp extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtEnterYourName;
+	
+	//Add window quote
+	private Quote windowQuote;
 
 	/**
 	 * Launch the application.
@@ -41,11 +45,15 @@ public class ForYouApp extends JFrame {
 	 */
 	public ForYouApp() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1285, 916);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		//Creation de la fenetre secondaire de l'auteur
+		windowQuote = new Quote();
+
+		
 		JLabel lblWelcome = new JLabel("WELCOME TO OUR PROJECT");
 		lblWelcome.setForeground(new Color(0, 206, 209));
 		lblWelcome.setFont(new Font("Century Gothic", Font.PLAIN, 54));
@@ -71,20 +79,20 @@ public class ForYouApp extends JFrame {
 		});
 		enterButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				String nom = txtEnterYourName.getText();
+				JOptionPane.showMessageDialog(null, "Welcome " + nom + " :) ~~~ !");	
+				if(!windowQuote.isVisible()) {
+					windowQuote.setVisible(true);
+				}
 			}
 		});
 		enterButton.setBounds(174, 600, 97, 25);
 		contentPane.add(enterButton);
 		
 		txtEnterYourName = new JTextField();
-		txtEnterYourName.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
 		txtEnterYourName.setForeground(new Color(0, 206, 209));
 		txtEnterYourName.setText("Enter your name");
-		txtEnterYourName.setBounds(351, 263, 150, 25);
+		txtEnterYourName.setBounds(349, 264, 150, 25);
 		contentPane.add(txtEnterYourName);
 		txtEnterYourName.setColumns(10);
 		
