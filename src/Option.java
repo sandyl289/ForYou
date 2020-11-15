85696fa5fedafa6d13deeefc35957f5bd0fdb6cb
KurooTT <74443534+KurooTT@users.noreply.github.com>
2020-11-15 12:27:29 -0500

    changed bounds for images

--- src/Option.java
+++ src/Option.java
@@ -128,15 +128,15 @@ public class Option extends JFrame {
 
 		// lblRocks = new JLabel(new ImageIcon("image/rocks.jpg"));
 		lblRocks = new JLabel(new ImageIcon("image/rocks.jpg"));
-		lblRocks.setBounds(297, 370, 653, 285);
+		lblRocks.setBounds(297, 348, 653, 307);
 		contentPane.add(lblRocks);
 
 		lblBeach = new JLabel(new ImageIcon("image/beach.jpg"));
-		lblBeach.setBounds(297, 311, 653, 344);
+		lblBeach.setBounds(297, 272, 653, 383);
 		contentPane.add(lblBeach);
 
 		lblEvening = new JLabel(new ImageIcon("image/evening.jpg"));
-		lblEvening.setBounds(297, 311, 653, 344);
+		lblEvening.setBounds(297, 272, 653, 383);
 		contentPane.add(lblEvening);
 
 		lblFengShui = new JLabel(new ImageIcon("image/feng-shui.jpg"));
@@ -144,11 +144,11 @@ public class Option extends JFrame {
 		contentPane.add(lblFengShui);
 
 		lblShell = new JLabel(new ImageIcon("image/shell.jpg"));
-		lblShell.setBounds(297, 285, 653, 370);
+		lblShell.setBounds(297, 259, 653, 396);
 		contentPane.add(lblShell);
 
 		lblSky = new JLabel(new ImageIcon("image/sky.jpg"));
-		lblSky.setBounds(297, 285, 653, 370);
+		lblSky.setBounds(297, 237, 653, 418);
 		contentPane.add(lblSky);
 
 		JButton btnBack = new JButton("Back");
